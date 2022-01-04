84a765b0d9c9afb8bc6149e2d50e99bbcc6e982e
Marco Dinarelli <marco.dinarelli@univ-grenoble-alpes.fr>
2022-01-04 16:43:36 +0100

    Updates to TALN 2022 submission

--- SLU/score_model_output.sh
+++ SLU/score_model_output.sh
@@ -5,6 +5,10 @@ source ${HOME}/work/tools/venv_python3.7.2_torch1.4${DECORE0_ID}/bin/activate
 script_path=${HOME}/work/tools/fairseq_tools/end2end_slu/scripts/
 
 model_output=$1
+aux_opt=""
+if [[ $# -ge 2 ]]; then
+	aux_opt=$2
+fi
 
 grep "^T\-" ${model_output} | cut -f 2- | perl -pe '{s/   / \# /g;}' > ${model_output}.ref # "s/   / \# \g;" is to keep into account spaces
 grep "^H\-" ${model_output} | cut -f 3- | perl -pe '{s/   / \# /g;}' > ${model_output}.hyp
@@ -13,7 +17,7 @@ grep "^P\-" ${model_output} | cut -f 2- > ${model_output}.scores
 # --clean-hyp removes blanks and duplicate tokens generated when training with CTC loss.
 # Remove this option if you trained the model with another loss (e.g. cross entropy)
 # --slu-out keeps only concepts from the raw output. Use this option if you want to score the model with Concept Error Rate (CER)
-python ${script_path}/compute_error_rate.py --slu-out --clean-hyp --ref ${model_output}.ref --hyp ${model_output}.hyp --slu-scores ${model_output}.scores
+python ${script_path}/compute_error_rate.py ${aux_opt} --clean-hyp --ref ${model_output}.ref --hyp ${model_output}.hyp --slu-scores ${model_output}.scores
 
 deactivate
 
